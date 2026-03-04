17584d21a73f1a6f63b908dff510e33d03bfef00
3Vivi <viviannelouize91@gmail.com>
2026-03-04 08:32:14 -0300

    Add files via upload

---
+++ correcao.py
@@ -0,0 +1,21 @@
+numero1 = float(input("Digite o 1° número: "))
+numero2 = float(input("Digite o 2° número: "))
+
+soma = numero1 + numero2 
+produto = numero1 * numero2
+
+print("Soma: ", soma)
+print("Produto: ", produto)
+
+
+numero = int(input("Digite um valor: "))
+
+if numero % 2 == 0:
+    resultado == numero ** 2
+else:
+    resultado = numero ** 3
+
+    print("Resultado: ", resultado)
+
+    usuario = input("Digite seu usuário: ")
+    senha = input()    
