02c185b64ca9791917592201301bdb62689e24c3
3Vivi <viviannelouize91@gmail.com>
2026-03-11 08:25:34 -0300

    Add files via upload

--- atividade.py
+++ atividade.py
@@ -1,53 +1,25 @@
-#Atividade - Manipulção de Listas em Python
-
-numeros = [10, 20, 30, 40, 20, 50]
-
-# 1) Quantos elementos existem na lista
-print("Quantidade de elementos:", len(numeros))
-
-# 2) Quantas vezes o número 20 aparece
-print("Quantas vezes 20 aparece:", numeros.count(20))
-
-# 3) Posição do número 30
-print("Posição do número 30:", numeros.index(30))
-
-# Verificar se 100 está na lista
-print("100 está na lista?", 100 in numeros)
+aluno = {}
 
 
-import random
+aluno["nome"] = input("Digite o nome do aluno: ")
+aluno["nota1"] = float(input("Digite a nota da prova 1: "))
+aluno["nota2"] = float(input("Digite a nota da prova 2: "))
 
-# Lista inicial
-numeros = [91, 34, 67, 15, 82]
+media = (aluno["nota1"] + aluno["nota2"]) / 2
 
-# Mostrar lista original
-print("Lista original:", numeros)
 
-# Ordem crescente
-numeros.sort()
-print("Ordem crescente:", numeros)
+aluno["media"] = media
 
-# Ordem decrescente
-numeros.sort(reverse=True)
-print("Ordem decrescente:", numeros)
 
-# Segunda lista
-dados = [80, 7, 10, 9, 19]
+print("\nDados do aluno:")
+for chave, valor in aluno.items():
+    print(f"{chave}: {valor}")
 
-# Embaralhar lista
-random.shuffle(dados)
-print("Lista embaralhada:", dados)
+if media >= 7:
+    situacao = "Aprovado"
+elif media >= 5:
+    situacao = "Recuperação"
+else:
+    situacao = "Reprovado"
 
-# Desafio
-lista3 = [5, 12, 8, 20, 3, 15]
-
-print("Terceira lista:", lista3)
-
-lista3.sort()
-print("Crescente:", lista3)
-
-lista3.sort(reverse=True)
-print("Decrescente:", lista3)
-
-random.shuffle(lista3)
-print("Embaralhada:", lista3)
+print("Situação do aluno:", situacao)
